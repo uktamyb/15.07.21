0742c1af830b9c4adc109e05335c426ed0a9e8db
UktamHudoyarov <uktamyb@gmail.com>
2021-07-17 21:13:38 +0500

    Yangi malumot qushildi

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-for (let i = 0; i <= 100; i++) {
-  if(i % 2 === 0 && i % 3 === 0 && i % 5 === 0){
-    console.log(i);
-  }
-  
-}
+var students = ["Uktam", "Anvar", "Ilxom", "Sarvar", "Barkamol", "Sevara", "Vohid", "Jumanazar"]
+
+// array
+// students.push("Uktam") - array oxiriga elemnt qushadi
+// students.unshift("Sardor") - arrayning boshiga element qushadi
+// students.pop - oxirgi elemntni olib tashlaydi
+// students.shift - boshidan elementni olib tashlaydi
+// students.slice - elementlarni kesib olib tashlaydi - students.slice(2, 5)
+// students.splice - kursatilgan element indexidan boshlab ushancha element kesib oladi. - students.splice(2, 5)
+// students[1] = "Javohir" - birinchi indexdagi elementni urniga Javohir yozib quyadi.
+// students.concat() - arraylarni bir-birigia qushadi.
+
+students[1] = "Javohir";
+// console.log(students);
+
+var str = "Uktam Hudoyarov";
+// console.log(str.split("")); // stringni arrayga ajratadi.
+// console.log(str.join("")); // arrayni stringa aylantiradi
+
+// console.log(students.sort());
+console.log(number.sort((a, b)=> a - b)); // raqamlarni sort qilish uchun sihlatiladi.
+console.log(number.sort((a, b)=> b - a)); // raqamlarni sort qilish uchun sihlatiladi. teskari
